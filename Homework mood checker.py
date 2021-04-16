2af53082f7d705297cabf3d3c06304670db62179
RicardoCosta2021 <81254358+RicardoCosta2021@users.noreply.github.com>
2021-04-16 11:01:22 +0100

    Add files via upload

---
+++ Homework mood checker.py
@@ -0,0 +1,23 @@
+title = "MOOD CHECKER"
+print(title)
+
+mood = input("Enter your mood today: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times!")
+elif mood== "sad":
+    print("Take a deep breath 4 times!")
+elif mood== "excited":
+    print("It is great to see you excited!")
+elif mood == "relaxed":
+    print("It is great to see you relaxed!")
+else:
+    print("I don't recognize this mood!")
+
+
+
+
+
+
