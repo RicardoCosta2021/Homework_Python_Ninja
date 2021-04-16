2e01ff91e21176f9d5b6a99c50e28d4eb14148ac
RicardoCosta2021 <81254358+RicardoCosta2021@users.noreply.github.com>
2021-04-16 11:01:49 +0100

    Add files via upload

---
+++ Homework secret Number.py
@@ -0,0 +1,20 @@
+title = "Secret Number"
+print(title)
+
+secret = 9
+
+num = int(input("Enter the secret number between 1 and 15: "))
+
+if num == secret:
+    print("Congratulations!!!")
+elif num == 8:
+    print("you are close!!!")
+elif num == 10:
+    print("you are close!!!")
+else:
+    print("Try Again!!!")
+
+
+
+
+
